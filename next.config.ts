6095dc7fe2f8bf1b432429cf138b96599c307419
pramanikankush <ankushpramanik@gmail.com>
2025-09-09 21:41:18 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: '/Career-Compass',
   eslint: {
     ignoreDuringBuilds: true
   },
